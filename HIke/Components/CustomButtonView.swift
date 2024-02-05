4338de92a65eb0eed4b67cbb46d9206f7e916192
smit-sk <patelsmit7008@gmail.com>
2024-02-05 15:50:58 -0500

    header-customBtn

---
+++ HIke/Components/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-05.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
