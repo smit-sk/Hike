e32cfe0aeff6f05bb6e354f77c9289bede246f70
smit-sk <patelsmit7008@gmail.com>
2024-02-05 15:17:52 -0500

    initial commit

--- HIke/Utility/ColorExtensions.swift
+++ HIke/Utility/ColorExtensions.swift
@@ -5,14 +5,16 @@
 //  Created by SMIT KHOKHARIYA on 2024-02-05.
 //
 
+import Foundation
 import SwiftUI
 
-struct ColorExtensions: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    ColorExtensions()
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmanLight = Color("ColorSalmanLight")
+    
 }
