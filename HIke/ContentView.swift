10e486cc229b9b8064a96430d39038ec6fd68f49
Smitkumar Khokhariya <patelsmit7008@gmail.com>
2024-02-05 14:29:43 -0500

    Initial Commit

---
+++ HIke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HIke/HIkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIkeApp.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-05.
+//
+
+import SwiftUI
+
+@main
+struct HIkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
