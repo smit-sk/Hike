d9b06e6225c11de272ca6ebc53193adc13e19cc5
smit-sk <patelsmit7008@gmail.com>
2024-02-05 20:26:45 -0500

    welcome-screen-with-animation

---
+++ HIke/Utility/GradientButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  GradientButtonStyle.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-05.
+//
+
+import SwiftUI
+
+struct GradientButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientButtonStyle()
+}
