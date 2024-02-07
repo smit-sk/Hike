2fc876035d60e458844a259494cdff055055ee3e
smit-sk <patelsmit7008@gmail.com>
2024-02-06 19:08:24 -0500

    motion-animation

---
+++ HIke/Components/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-06.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
