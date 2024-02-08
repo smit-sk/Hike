62d37fc1e1616c2a065e7e3109dcddb5ef88872e
smit-sk <patelsmit7008@gmail.com>
2024-02-07 21:07:38 -0500

    bottom-sheet-reusableComponent-listitem

---
+++ HIke/Components/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-07.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
---
+++ HIke/Screen/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  HIke
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-06.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
